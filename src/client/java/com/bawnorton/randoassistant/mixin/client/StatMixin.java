b2fdc882b11ac403abbefd3a701a5b6fddd443d6
Benjamin-Norton <bawnorton@me.com>
2023-05-12 17:14:44 -0400

    fix #16

--- src/client/java/com/bawnorton/randoassistant/mixin/client/StatMixin.java
+++ src/client/java/com/bawnorton/randoassistant/mixin/client/StatMixin.java
@@ -1,18 +1,16 @@
 package com.bawnorton.randoassistant.mixin.client;
 
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.registry.Registry;
 import net.minecraft.stat.Stat;
 import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(Stat.class)
 public abstract class StatMixin {
-    @WrapOperation(method = "getName(Lnet/minecraft/stat/StatType;Ljava/lang/Object;)Ljava/lang/String;", at = @At(value = "INVOKE", target = "Lnet/minecraft/registry/Registry;getId(Ljava/lang/Object;)Lnet/minecraft/util/Identifier;", ordinal = 1))
-    private static <T> Identifier getName(Registry<T> registry, T object, Operation<Identifier> original) {
-        return object instanceof Identifier ? (Identifier) object : original.call(registry, object);
+    @Redirect(method = "getName(Lnet/minecraft/stat/StatType;Ljava/lang/Object;)Ljava/lang/String;", at = @At(value = "INVOKE", target = "Lnet/minecraft/registry/Registry;getId(Ljava/lang/Object;)Lnet/minecraft/util/Identifier;", ordinal = 1))
+    private static <T> Identifier getName(Registry<T> registry, T t) {
+        return t instanceof Identifier ? (Identifier) t : registry.getId(t);
     }
 }
